f1f417d7d232b4ff84816fcbd0b15d0ed6ea5888
Tim Acosta <timacosta06@gmail.com>
2021-02-13 17:25:34 +0000

    photograph entity added

---
+++ NotebooksApp/Model/PhotographMO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PhotographMO+CoreDataClass.swift
+//  NotebooksApp
+//
+//  Created by Tim Acosta on 13/2/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PhotographMO)
+public class PhotographMO: NSManagedObject {
+
+}
